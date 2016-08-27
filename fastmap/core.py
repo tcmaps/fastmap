6e34559e5eaebbaabcae29a6655800d654fc4ab9
Tr4sHCr4fT <Tr4sHCr4fT@users.noreply.github.com>
2016-08-27 14:09:24 +0200

    threading.

--- fastmap/core.py
+++ fastmap/core.py
@@ -1,65 +1,20 @@
-import sys, time, logging
+import logging
 import sqlite3
 
-from threading import Thread
 from s2sphere import CellId, LatLng
-from fastmap.apiwrap import api_init, get_response, Status3Exception
-from fastmap.utils import set_bit, get_cell_ids, sub_cells_normalized
-from pgoapi.exceptions import NotLoggedInException
-
+from fastmap.utils import set_bit
 log = logging.getLogger(__name__)
 
-class FastMapWorker(Thread):
+def process_response(db, response_dict):
     
-    def __init__(self, threadID, config, account, work, lock):
-        Thread.__init__(self)
-        self.threadID = threadID
-        self.config = config
-        self.lock = lock
-        self.config.username = account.username
-        self.config.password = account.password
-        self.workload = work
-        self.pos = 0
-        self.stats = [0, 0, 0]
-        self.name = '(%2d)' % self.threadID
+    stats = [0, 0, 0]
+    dbc = db.cursor()
     
-    def run(self):
-        
-        if len(self.workload) == 0: return
-
-        db = sqlite3.connect(self.config.dbfile)
-
-        self.api = api_init(self.config)
-        if self.api == None:   
-            log.error('Login failed!'); return
-        log.info(self.name + " Logged into account '%s' " % self.config.username)
-        time.sleep(5)
-
-        for work in self.workload:
-            
-            log.info(self.name + ' Cell %d of %d.' % (self.pos+1,len(self.workload)))         
-            cell = CellId.from_token(work)
-            lat = CellId.to_lat_lng(cell).lat().degrees 
-            lng = CellId.to_lat_lng(cell).lng().degrees
-            cell_ids = get_cell_ids(sub_cells_normalized(cell, level=15))
-            
-            try:
-                response_dict = get_response(self.api, cell_ids, lat, lng)
-            except Status3Exception:
-                ('Worker %d down: Banned' % self.threadID); return
-            except NotLoggedInException:
-                self.api = None
-                self.api = api_init(self.config)
-                continue
-            except: self.log.error(sys.exc_info()[0]); continue         
-        
-            stats = [0, 0, 0]
-            
+    if response_dict:
+        if 'responses' in response_dict:
             for map_cell in response_dict['responses']['GET_MAP_OBJECTS']['map_cells']:
                 cellid = CellId(map_cell['s2_cell_id']).to_token()
                 content = 0                      
-                
-                dbc = db.cursor()
                 
                 if 'forts' in map_cell:
                     for fort in map_cell['forts']:
@@ -93,23 +48,14 @@ class FastMapWorker(Thread):
                         
                 dbc.execute("INSERT OR IGNORE INTO cells (cell_id, content, last_scan) "
                 "VALUES ('{}', {}, {})".format(cellid,content,int(map_cell['current_timestamp_ms']/1000)))
+                log.debug(' got {} Gyms, {} Pokestops, {} Spawns.'.format(*stats))
                 #self.stats[0] += stats[0]; self.stats[1] += stats[1]; self.stats[2] += stats[2]
-            
-            
-            dbc.execute("DELETE FROM _queue WHERE cell_id='{}'".format(work))
-                    
-            # LOCK
-            self.lock.acquire()
-            db.commit()
-            self.lock.release()
-            # UNLOCK
-        
-            self.pos += 1    
-            time.sleep(int(self.config.delay))
-              
-            log.debug(self.name+' got {} Gyms, {} Pokestops, {} Spawns.'.format(*stats))
-        
-        
-        #log.info(self.name+' Scanned {} cells; got {} Gyms, {} Pokestops, {} Spawns.'.format(self.pos,*self.stats))
+    
+            return stats
+    
+    return None
 
-        return stats
+            
+def pop_que(db, que):
+    
+    db.cursor().execute("DELETE FROM _queue WHERE cell_id='{}'".format(que)); db.commit()                      
