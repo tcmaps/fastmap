1467468cedcfcda68ec23e0ffabba83d16830677
TC <n23@arcor.de>
2016-08-27 20:56:21 +0200

    smaller db size

--- fastmap/db.py
+++ fastmap/db.py
@@ -25,16 +25,16 @@ def fill_db(dbfile, cells):
 def create_db(dbfile):
     db = sqlite3.connect(dbfile); dbc = db.cursor()
     dbc.execute("CREATE TABLE _config (version VARCHAR DEFAULT '1.0')")
-    dbc.execute("CREATE TABLE _queue (cell_id VARCHAR PRIMARY KEY, cell_level INT)")
-    dbc.execute("CREATE TABLE cells (cell_id VARCHAR PRIMARY KEY, content INT, last_scan TIMESTAMP)")
+    dbc.execute("CREATE TABLE _queue (cell_id VARCHAR PRIMARY KEY, cell_level INT) WITHOUT ROWID")
+    dbc.execute("CREATE TABLE cells (cell_id VARCHAR PRIMARY KEY, content INT, last_scan TIMESTAMP) WITHOUT ROWID")
     dbc.execute("CREATE TABLE forts (fort_id VARCHAR PRIMARY KEY, cell_id VARCHAR, \
     pos_lat DOUBLE, pos_lng DOUBLE, fort_enabled BOOLEAN, fort_type INT, fort_description TEXT, \
     fort_image BLOB, fort_sponsor TEXT, fort_last_modified TIMESTAMP, last_scan TIMESTAMP,\
-    FOREIGN KEY ( cell_id ) REFERENCES cells (cell_id) )")
+    FOREIGN KEY ( cell_id ) REFERENCES cells (cell_id) ) WITHOUT ROWID")
     dbc.execute("CREATE TABLE spawns (spawn_id VARCHAR PRIMARY KEY, cell_id VARCHAR, \
     pos_lat DOUBLE, pos_lng DOUBLE, static_spawner INT DEFAULT (0), nest_spawner INT DEFAULT (0), \
     spawn_time_base TIME, spawn_time_offset TIME, spawn_time_dur TIME, last_scan TIMESTAMP, \
-    FOREIGN KEY (cell_id) REFERENCES cells (cell_id) )")
+    FOREIGN KEY (cell_id) REFERENCES cells (cell_id) ) WITHOUT ROWID")
     dbc.execute("INSERT INTO _config (version) VALUES (%s)" % FMDBVERSION); db.commit()
     log.info('DB created!')
 
