a384f24b81e4f02478110880ef7d023652b2f2be
TC <n23@arcor.de>
2016-08-29 19:31:43 +0200

    because oop rocks.

--- fm/core.py
+++ fm/core.py
@@ -1,9 +1,122 @@
-import logging
+#!/usr/bin/env python
 
-log = logging.getLogger(__name__)
+import logging
+from threading import Thread
 
 
 class Work():
     def __init__(self, reference, content):
         self.index = reference      
         self.work = content
+        
+class Params():
+        def __init__(self, paramdict):
+            self.params = paramdict      
+
+class PoisonPill(Exception):
+        def __init__(self, broadcast=False):
+            self.relay = broadcast
+            pass
+
+
+class Mastermind(Thread):
+    
+    def __init__(self, threadID, config=None, worklist=None, params=None, signals=None, ):
+        Thread.__init__(self)
+        self.threadID = threadID
+        self.config = config
+        self.parameters = params
+        self.workload = worklist   
+        self.control = signals     
+        self.pos = 0
+        self.name = '[Master %2d]' % self.threadID
+        self.log = logging.getLogger(self.name)
+        
+    def run(self):
+        pass
+
+class Minion(Thread):
+    
+    # generic constructor
+    def __init__(self, threadID=0, config=None, workin=None, workout=None,\
+                       params=None, signals=None, locks=None, events=None):
+        
+        Thread.__init__(self)
+        self.threadID = threadID
+        
+        self.preinit()
+        
+        self.config = config
+        self.parameters = params
+        self.input = workin        
+        self.output = workout
+        self.control = signals
+        self.locks = locks
+        self.events = events
+        self.work = None
+        self.runs = 1
+        self.pos = 0
+        self.name = '[Minion %2d]' % self.threadID
+        self.log = logging.getLogger(self.name)
+        
+        self.postinit()
+    
+    
+    # generic start, run and stop handling
+    def run(self):
+        
+        # run this once
+        self.firstrun()
+        
+        while self.runs:
+            
+            # optionaly wait for events
+            self.wait()
+            
+            # fetch queue
+            self.work = self.input.get()
+            
+            # sanity check
+            if self.work == None:
+                continue
+            
+            # broadcast to other threads
+            if hasattr(self.work, 'relay'):
+                if self.work.relay is True:
+                    self.input.put(self.work)
+            
+            # stop if received
+            if self.work is PoisonPill:
+                self.runs = False
+                break
+            
+            # put back work if fails
+            try: self.main()
+            except Exception as e:
+                self.input.put(self.work)
+                self.log.error(e)
+                
+        self.cleanup()
+        
+        return
+
+
+    # override these to customize
+    
+    def preinit(self):
+        pass    
+    
+    def postinit(self):
+        pass
+
+    def firstrun(self):
+        pass
+    
+    def wait(self):
+        pass
+
+    def main(self):
+        pass
+    
+    def cleanup(self):
+        pass
