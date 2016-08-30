d98da5bfd8146ba1c0725a9d5a3d96ec7f6c3d05
Tr4sHCr4fT <Tr4sHCr4fT@users.noreply.github.com>
2016-08-30 17:26:04 +0200

    Revert "Threading Core Rewrite"

--- fastmap/apiwrap.py
+++ fastmap/apiwrap.py
@@ -15,12 +15,6 @@ log = logging.getLogger(__name__)
 class Status3Exception(Exception):
     pass
 
-class PoGoAccount():
-    def __init__(self, auth, login, passw):
-        self.auth_service = auth
-        self.username = login
-        self.password = passw
-
 def api_init(account):
     api = PGoApi()
 
@@ -39,14 +33,14 @@ def api_init(account):
                 log.error('Account banned!'); return None
             else: return None
 
+
 def get_response(api, cell_ids, lat, lng, alt=0):
     
     timestamps = [0,] * len(cell_ids)
     response_dict = []
     delay = 11
-    trys = 3
     
-    while trys:
+    while True:
         api.set_position(lat, lng, alt)
         response_dict = api.get_map_objects(latitude=lat, longitude=lng, since_timestamp_ms = timestamps, cell_id = cell_ids)
         if response_dict:
@@ -60,9 +54,6 @@ def get_response(api, cell_ids, lat, lng, alt=0):
 
         time.sleep(delay)
         delay += 5
-        trys -= 1
-    
-    return None
 
 def get_encryption_lib_path():
     # win32 doesn't mean necessarily 32 bits
@@ -75,10 +66,10 @@ def get_encryption_lib_path():
     elif sys.platform == "darwin":
         lib_name = "libencrypt-osx-64.so"
 
-    elif os.uname()[4].startswith("arm") and platform.architecture()[0] == '32bit':  # @UndefinedVariable
+    elif os.uname()[4].startswith("arm") and platform.architecture()[0] == '32bit':
         lib_name = "libencrypt-linux-arm-32.so"
 
-    elif os.uname()[4].startswith("aarch64") and platform.architecture()[0] == '64bit':  # @UndefinedVariable
+    elif os.uname()[4].startswith("aarch64") and platform.architecture()[0] == '64bit':
         lib_name = "libencrypt-linux-arm-64.so"
 
     elif sys.platform.startswith('linux'):
--- fastmap/worker.py
+++ fastmap/worker.py
@@ -1,162 +1,118 @@
-import time, logging, sqlite3
+import sys, time, logging
+import sqlite3
 
-from time import sleep
+from threading import Thread
 from s2sphere import CellId, LatLng
-from sqlite3 import IntegrityError, ProgrammingError, DataError
-from sqlite3 import OperationalError, InterfaceError, DatabaseError
-
-from fastmap.core import Work, Minion, PoisonPill
 from fastmap.apiwrap import api_init, get_response, Status3Exception
 from fastmap.utils import set_bit, get_cell_ids, sub_cells_normalized
-from pgoapi.exceptions import AuthException, NotLoggedInException
+from pgoapi.exceptions import NotLoggedInException
 
 log = logging.getLogger(__name__)
 
-
-class MapRequest(Minion):
+class FastMapWorker(Thread):
     
-    def preinit(self):
-        self.name = '[Req]'
-    
-    def runfirst(self):
+    def __init__(self, threadID, config, account, work, lock):
+        Thread.__init__(self)
+        self.threadID = threadID
+        self.config = config
+        self.lock = lock
+        self.config.username = account.username
+        self.config.password = account.password
+        self.workload = work
+        self.pos = 0
+        self.stats = [0, 0, 0]
+        self.name = '(%2d)' % self.threadID
         
-        self.api = api_init(self.parameters)
+        self.api = api_init(self.config)
         if self.api == None:   
-            self.log.error(self.name + 'Login failed!')
-            raise AuthException
-        
-        self.log.info(self.name + " Logged into account '%s' " % self.parameters.username)
-        
-        sleep(3)
+            log.error('Login failed!'); return
+        log.info(self.name + " Logged into account '%s' " % self.config.username)
     
-    def main(self):
+    def run(self):
         
-        log = self.log
-        work = self.work
-            
-        log.debug(self.name + ' does Cell %s.' % (work.index))         
-        cell = CellId.from_token(work.work)
-        lat = CellId.to_lat_lng(cell).lat().degrees 
-        lng = CellId.to_lat_lng(cell).lng().degrees
-        cell_ids = get_cell_ids(sub_cells_normalized(cell, level=15))
-        
-        try:
-            response_dict = get_response(self.api, cell_ids, lat, lng)
-        except Status3Exception:
-            log.critical('Worker %d down: Banned' % self.threadID); return
-        except NotLoggedInException:
-            self.input.put(work)                
-            self.api = None
-            self.api = api_init(self.config)
-        except Exception as e:
-            self.log.error(e); return         
-        else:
-            self.output.put(Work(work.index,response_dict))
-        
-        finally:
-            if response_dict is None:
-                self.input.put(work)
-        
-        time.sleep(self.config.delay)
+        if len(self.workload) == 0: return
 
+        db = sqlite3.connect(self.config.dbfile)
+
+        for work in self.workload:
             
-class MapParse(Minion):
-      
-    def preinit(self):
-        self.name = '[Map]'
-      
-    def main(self):
-        
-        log = self.log
-        work = self.work
-        
-        stats = [0, 0, 0]
-        querys = []
-        
-        for map_cell in work.work['responses']['GET_MAP_OBJECTS']['map_cells']:
-            cellid = CellId(map_cell['s2_cell_id']).to_token()
-            content = 0                      
+            log.info(self.name + ' Cell %d of %d.' % (self.pos+1,len(self.workload)))         
+            cell = CellId.from_token(work)
+            lat = CellId.to_lat_lng(cell).lat().degrees 
+            lng = CellId.to_lat_lng(cell).lng().degrees
+            cell_ids = get_cell_ids(sub_cells_normalized(cell, level=15))
             
-            if 'forts' in map_cell:
-                for fort in map_cell['forts']:
-                    if 'gym_points' in fort:
-                        stats[0]+=1
-                        content = set_bit(content, 2)
-                        querys.append("INSERT OR IGNORE INTO forts (fort_id, cell_id, pos_lat, pos_lng, fort_enabled, fort_type, last_scan) "
-                        "VALUES ('{}','{}',{},{},{},{},{})".format(fort['id'],cellid,fort['latitude'],fort['longitude'], \
-                        int(fort['enabled']),0,int(map_cell['current_timestamp_ms']/1000)))
-                    else:
-                        stats[1]+=1
-                        content = set_bit(content, 1)
-                        querys.append("INSERT OR IGNORE INTO forts (fort_id, cell_id, pos_lat, pos_lng, fort_enabled, fort_type, last_scan) "
-                        "VALUES ('{}','{}',{},{},{},{},{})".format(fort['id'],cellid,fort['latitude'],fort['longitude'], \
-                        int(fort['enabled']),1,int(map_cell['current_timestamp_ms']/1000)))
-                                                             
-            if 'spawn_points' in map_cell:
-                content = set_bit(content, 0)
-                for spawn in map_cell['spawn_points']:
-                    stats[2]+=1;
-                    spwn_id = CellId.from_lat_lng(LatLng.from_degrees(spawn['latitude'],spawn['longitude'])).parent(20).to_token()
-                    querys.append("INSERT OR IGNORE INTO spawns (spawn_id, cell_id, pos_lat, pos_lng, last_scan) "
-                    "VALUES ('{}','{}',{},{},{})".format(spwn_id,cellid,spawn['latitude'],spawn['longitude'],int(map_cell['current_timestamp_ms']/1000)))
-            if 'decimated_spawn_points' in map_cell:
-                content = set_bit(content, 0)
-                for spawn in map_cell['decimated_spawn_points']:
-                    stats[2]+=1;
-                    spwn_id = CellId.from_lat_lng(LatLng.from_degrees(spawn['latitude'],spawn['longitude'])).parent(20).to_token()
-                    querys.append("INSERT OR IGNORE INTO spawns (spawn_id, cell_id, pos_lat, pos_lng, last_scan) "
-                    "VALUES ('{}','{}',{},{},{})".format(spwn_id,cellid,spawn['latitude'],spawn['longitude'],int(map_cell['current_timestamp_ms']/1000)))
+            try:
+                response_dict = get_response(self.api, cell_ids, lat, lng)
+            except Status3Exception:
+                ('Worker %d down: Banned' % self.threadID); return
+            except NotLoggedInException:
+                self.api = None
+                self.api = api_init(self.config)
+                continue
+            except: self.log.error(sys.exc_info()[0]); continue         
+        
+            stats = [0, 0, 0]
+            querys = []
+            
+            for map_cell in response_dict['responses']['GET_MAP_OBJECTS']['map_cells']:
+                cellid = CellId(map_cell['s2_cell_id']).to_token()
+                content = 0                      
+                
+                if 'forts' in map_cell:
+                    for fort in map_cell['forts']:
+                        if 'gym_points' in fort:
+                            stats[0]+=1
+                            content = set_bit(content, 2)
+                            querys.append("INSERT OR IGNORE INTO forts (fort_id, cell_id, pos_lat, pos_lng, fort_enabled, fort_type, last_scan) "
+                            "VALUES ('{}','{}',{},{},{},{},{})".format(fort['id'],cellid,fort['latitude'],fort['longitude'], \
+                            int(fort['enabled']),0,int(map_cell['current_timestamp_ms']/1000)))
+                        else:
+                            stats[1]+=1
+                            content = set_bit(content, 1)
+                            querys.append("INSERT OR IGNORE INTO forts (fort_id, cell_id, pos_lat, pos_lng, fort_enabled, fort_type, last_scan) "
+                            "VALUES ('{}','{}',{},{},{},{},{})".format(fort['id'],cellid,fort['latitude'],fort['longitude'], \
+                            int(fort['enabled']),1,int(map_cell['current_timestamp_ms']/1000)))
+                                                                 
+                if 'spawn_points' in map_cell:
+                    content = set_bit(content, 0)
+                    for spawn in map_cell['spawn_points']:
+                        stats[2]+=1;
+                        spwn_id = CellId.from_lat_lng(LatLng.from_degrees(spawn['latitude'],spawn['longitude'])).parent(20).to_token()
+                        querys.append("INSERT OR IGNORE INTO spawns (spawn_id, cell_id, pos_lat, pos_lng, last_scan) "
+                        "VALUES ('{}','{}',{},{},{})".format(spwn_id,cellid,spawn['latitude'],spawn['longitude'],int(map_cell['current_timestamp_ms']/1000)))
+                if 'decimated_spawn_points' in map_cell:
+                    content = set_bit(content, 0)
+                    for spawn in map_cell['decimated_spawn_points']:
+                        stats[2]+=1;
+                        spwn_id = CellId.from_lat_lng(LatLng.from_degrees(spawn['latitude'],spawn['longitude'])).parent(20).to_token()
+                        querys.append("INSERT OR IGNORE INTO spawns (spawn_id, cell_id, pos_lat, pos_lng, last_scan) "
+                        "VALUES ('{}','{}',{},{},{})".format(spwn_id,cellid,spawn['latitude'],spawn['longitude'],int(map_cell['current_timestamp_ms']/1000)))
+                        
+                querys.append("INSERT OR IGNORE INTO cells (cell_id, content, last_scan) "
+                "VALUES ('{}', {}, {})".format(cellid,content,int(map_cell['current_timestamp_ms']/1000)))
+                #self.stats[0] += stats[0]; self.stats[1] += stats[1]; self.stats[2] += stats[2]
+            
+            
+            querys.append("DELETE FROM _queue WHERE cell_id='{}'".format(work))
                     
-            querys.append("INSERT OR IGNORE INTO cells (cell_id, content, last_scan) "
-            "VALUES ('{}', {}, {})".format(cellid,content,int(map_cell['current_timestamp_ms']/1000)))
-
-        log.debug(self.name + ' got {} Gyms, {} Pokestops, {} Spawns.'.format(*stats))
-    
-        self.output.put(Work(work.index,querys)) 
-
-        
-class DataBase(Minion):
-    
-    def preinit(self):
-        self.name = '[DB]'
-        
-    def postinit(self):
-        self.lock = self.locks[0]
-    
-    def runfirst(self):
-        self.db = sqlite3.connect(self.config.dbfile)
-
-    def main(self):
-        
-        log = self.log
-        work = self.work
-        
-        dbc = self.db.cursor()
-        
-        # 100% empty cell?
-        if len(work.work) == 0:
-            work.work = ["UPDATE _queue SET scan_status=2 WHERE cell_id='%s'" % work.index]
-            log.warning(self.name + ' Cell %s returned empty!' % work.index)
+            # LOCK
+            self.lock.acquire()
             
-        try:
-            self.lock.acquire()     
-            for query in work.work:
+            dbc = db.cursor()
+            for query in querys:
                 dbc.execute(query)
-            dbc.execute("UPDATE _queue SET scan_status=1 WHERE cell_id='%s'" % work.index)
-            self.db.commit()
-        
-        except (IntegrityError, ProgrammingError, DataError): 
-            self.db.rollback()
-            dbc.execute("UPDATE _queue SET scan_status=3 WHERE cell_id='%s'" % work.index)
-            self.output.put(Work(work.index,False)); sleep(1)
-            log.error(self.name + ' Upsert for Cell %s failed!' % work.index)
-        
-        except (OperationalError, InterfaceError, DatabaseError):
-            self.output.put(PoisonPill())
-            return # Shut down DB
-        
-        else:
-            self.output.put(Work(work.index,True))                    
-            log.debug(self.name + ' inserted %d Querys.' % len(work.work))
+            db.commit()
             
-        finally: self.lock.release()
+            self.lock.release()
+            # UNLOCK
+        
+            self.pos += 1    
+            time.sleep(int(self.config.delay))
+              
+            log.debug(self.name+' got {} Gyms, {} Pokestops, {} Spawns.'.format(*stats))
+        
+        
+        #log.info(self.name+' Scanned {} cells; got {} Gyms, {} Pokestops, {} Spawns.'.format(self.pos,*self.stats))
+
+        return stats
